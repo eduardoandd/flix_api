09731ddda9047bfd3bedc9f9162ee9bc9ba45a12
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-10 14:07:51 -0300

    endpoint get by id

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view,name='genre')
+    path('genres/', genre_create_list_view,name='genre_create_list'),
+    path('genres/', genre_create_list_view,name='genre_create_list'),
+    path('genres/<int:pk>/', genre_detail_view,name='genre_datail_view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -2,12 +2,13 @@ from django.http import JsonResponse
 from genres.models import Genre
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
 
 
 # END POINT 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     
     if request.method == 'GET':
         genres = Genre.objects.all() # SELECT * FROM GENRES
@@ -23,6 +24,11 @@ def genre_view(request):
         
         return JsonResponse({'id': new_genre.id, 'name':new_genre.name},status=201)
         
-
-
+@csrf_exempt
+def genre_detail_view(request, pk):
+    # genre = Genre.objects.get(pk=pk)
+    genre= get_object_or_404(Genre,pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    
+    return JsonResponse(data)
 
