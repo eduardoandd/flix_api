de562f3b6aee6d94f7ee47b194715bd553c72b9d
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-13 17:55:26 -0300

    fechando endpoints

--- genres/views.py
+++ genres/views.py
@@ -5,14 +5,17 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from genres.serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
 
 #GET ALL AND POST
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes=IsAuthenticated
     queryset=Genre.objects.all()
     serializer_class=GenreSerializer
 
 # GET BY ID, PUT, DELETE
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=IsAuthenticated
     queryset=Genre.objects.all()
     serializer_class= GenreSerializer
     
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from movies.models import *
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from movies.serializers import *
+from movies.models import *
+
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes=(IsAuthenticated,)
     queryset= Movie.objects.all()
     serializer_class= MovieModelSerializer
     
 class MovieRetrieverUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthenticated)
     queryset=Movie.objects.all()
     serializer_class=MovieModelSerializer
     
