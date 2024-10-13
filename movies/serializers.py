f361b6cabf52b15584d76ed9c75aec87ac22ac67
eduardoandd <eduardo.andrade.dev@gmail.com>
2024-10-13 15:11:00 -0300

    calculando média de avaliação dos filmes

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,18 +6,35 @@ from rest_framework import serializers
     
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    
+    rate = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model=Movie
         fields='__all__'
+        
+    def get_rate(self,obj):
+        reviews= obj.review.all()
+        
+        if reviews:
+            sum_reviews=0
+            for review in reviews:
+                sum_reviews +=review.stars          
+                
+            mean= round(sum_reviews / reviews.count(),1)
+            
+            return mean
     
     def validate_realease_data(self,value):
-        if value.year <1990:
+        if value.year <1930:
             raise serializers.ValidationError('A dara de lançamento não pode ser inferior a 1990.')
         
         return value
     
     def validate_resume(self,value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError('Resumo limitado á 200 caracteres.')
+        
+    
         
         
